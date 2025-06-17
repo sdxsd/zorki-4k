488434d3496e064e3a964eca9ed1de111c477b7e
sdxsd <willmaguire@icloud.com>
2025-06-17 22:06:05 +0200

    More tests.

--- tester.sh
+++ tester.sh
@@ -3,15 +3,48 @@
 CLEAR="\033[0m"
 RED="\033[0;31m"
 GREEN="\033[0;32m"
+shellcode_execution_success="Hello World!"
+no_shellcode_executed_success=""
 
 function print_result () {
-    if [ "$1" == "Hello World!" ]; then
+    if [ "$1" == "$5" ]; then
         printf "[$2 $3]: ${GREEN} Success! ${CLEAR}\n"
     elif [ $4 -eq 139 ]; then # Segfault code.
         printf "[$2 $3]: ${RED} Failure! (Segfault) ${CLEAR}\n"
     else
         printf "[$2 $3]: ${RED} Failure! $4 ${CLEAR}\n"
     fi
+}
+
+function test_large_random_files_no_shellcode () {
+    testdir="testfiles/large-random-files"
+    filecount=4
+    name="Large Random File No Shellcode"
+
+    mkdir -p $testdir
+    rm $testdir/*.infectable
+    for ((i=1; i <= filecount; i++)); do
+        touch -f "$testdir/file$i.infectable"
+        dd if=/dev/urandom of="$testdir/files$i.infectable" count=10240 &> /dev/null
+    done
+    result=$(./loader $testdir)
+    print_result "$result" "$filecount" "$name" "$?" "$no_shellcode_execution_success"
+
+}
+
+function test_normal_files_no_shellcode () {
+    testdir="testfiles/normal-files-no-shellcode"
+    filecount=8
+    name="Normal Files No Shellcode"
+
+    mkdir -p $testdir
+    rm $testdir/*.infectable
+    for ((i=1; i <= filecount; i++)); do
+        touch -f "$testdir/file$i.infectable"
+        echo "This is a normal file, with lots of shellcode and evil binary data. I swear!!" > "$testdir/files$i.infectable"
+    done
+    result=$(./loader $testdir)
+    print_result "$result" "$filecount" "$name" "$?" "$no_shellcode_execution_success"
 }
 
 function test_normal_files_with_text () {
@@ -27,7 +60,7 @@ function test_normal_files_with_text () {
     done
     ./infector $testdir shellcode &> /dev/null
     result=$(./loader $testdir)
-    print_result "$result" "$filecount" "$name" "$?"
+    print_result "$result" "$filecount" "$name" "$?" "$shellcode_execution_success"
 }
 
 function test_large_random_files () {
@@ -43,7 +76,7 @@ function test_large_random_files () {
     done
     ./infector $testdir shellcode &> /dev/null
     result=$(./loader $testdir)
-    print_result "$result" "$filecount" "$name" "$?"
+    print_result "$result" "$filecount" "$name" "$?" "$shellcode_execution_success"
 }
 
 function test_normal_empty_files () {
@@ -58,7 +91,7 @@ function test_normal_empty_files () {
     done
     ./infector $testdir shellcode &> /dev/null
     result=$(./loader $testdir)
-    print_result "$result" "$filecount" "$name" "$?"
+    print_result "$result" "$filecount" "$name" "$?" "$shellcode_execution_success"
 }
 
 function test_all () {
@@ -66,6 +99,8 @@ function test_all () {
         test_normal_empty_files
         test_large_random_files
         test_normal_files_with_text
+        test_normal_files_no_shellcode
+        test_large_random_files_no_shellcode
     )
     results=()
     for test in ${tests[@]};
